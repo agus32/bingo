7d05de521f784e3b69ea13a7d1a555cc1b643aad
Agustin <agus@example.com>
2020-04-16 17:38:44 -0300

    Testeado columnas no vacias

---
+++ tests/test_columna.py
@@ -0,0 +1,12 @@
+from src.bingo import carton
+
+def test_columna():
+    mi_carton = carton()
+    j = 0
+    contador=0 
+    for j in range(9):
+        suma= sum([fila[j] for fila in mi_carton])
+	if suma > 0:
+		contador = contador+1
+# contador suma 1 por cada columna no vacía 
+    assert contador == 9
