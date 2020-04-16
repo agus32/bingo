0cdeeef11b54250c9a1879ce8a49bd3568e377ef
Agustin <agus@example.com>
2020-04-16 16:55:03 -0300

    Agregado funcion carton y testeado que no sea menor a 15 las celdas

---
+++ src/bingo.py
@@ -0,0 +1,11 @@
+
+# Los 0 representan celdas vacias en el carton.
+# Los 1 representan celdas ocupadas en el carton.
+
+def carton():
+    mi_carton = (
+        (1,0,0,1,1,0,1,0,1),
+        (0,1,0,1,0,1,1,1,1),
+        (0,1,0,0,1,1,0,1,0)
+    )
+    return mi_carton
---
+++ tests/test_no_menor.py
@@ -0,0 +1,11 @@
+from src.bingo import carton
+
+def test_contar_celdas_ocupadas():
+    mi_carton = carton()
+    contador = 0
+    for fila in mi_carton:
+        for celda in fila:
+            contador = contador + celda
+    
+    # Esperamos encontrar 15 celdas ocupadas.
+    assert contador >= 15
