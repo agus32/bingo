4bd918e941c9e09bcde08994428a8b17753d1f2d
agus <agus@example.com>
2020-05-26 15:52:00 -0300

    nuevo test en una fila no mas de dos celdas vacias seguidas

---
+++ tests/test_no_mas_2celdas_vacias_cons.py
@@ -0,0 +1,14 @@
+from src.bingo import carton
+
+def test_nomas_2celdas_vacias_cons():
+    mi_carton = carton()
+    celda_vacia = 0
+    contador = 0
+    for fila in range(0, 3):
+        for columna in range(0, 9):
+            if mi_carton[fila][columna] == celda_vacia:
+                contador = contador+1
+            else:
+                assert contador <= 2
+                contador = 0
+        contador = 0
